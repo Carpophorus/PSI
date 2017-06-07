ad6bf7af522e963c418630de5e634cece89ddb58
Marko Mrkonjić <markomrkonjic9@gmail.com>
2017-06-07 20:31:17 +0200

    index.php
    treba da bued u nekom _partial, ispraviti href na odgovarajuće

--- IMPL/src/Psi/AppBundle/Resources/views/App/index.html.php
+++ IMPL/src/Psi/AppBundle/Resources/views/App/index.html.php
@@ -1,1 +1,14 @@
-
+<a href="search.php" id="index-left">
+  <span></span>
+  <div>
+    SEARCH
+  </div>
+</a>
+<a href="loginregistersignup.php" id="index-right">
+  <span></span>
+  <div>
+    LOGIN
+    <br>REGISTER
+    <br>SIGN&nbsp;UP
+  </div>
+</a>
