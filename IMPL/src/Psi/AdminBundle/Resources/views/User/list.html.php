3684cff5e0759f30fee7aaf2bf410b974e2cdabb
Marko Mrkonjić <markomrkonjic9@gmail.com>
2017-06-12 23:05:33 +0200

    admin users and new user done

--- IMPL/src/Psi/AdminBundle/Resources/views/User/list.html.php
+++ IMPL/src/Psi/AdminBundle/Resources/views/User/list.html.php
@@ -3,12 +3,13 @@
 <?php //renderTable($heading, $data, $cssId, $cssClass, $actions) ?>
 
 <?php $view['UI']->start('_content') ?>
+<div id="adu-bckgrnd"></div>
 <?php
 
 $heading = ["Id",'Email', 'SummonerName', 'Firstname', 'Lastname', 'Status'];
 
 echo $view['datatable']->renderTable(
-    $heading, $userData, 'userListTable', 'data-table', [ 
+    $heading, $userData, 'userListTable', 'data-table', [
         'edit' => [
             'name' => "edit",
             'jsMethod' => "AdminUI.editUser",
