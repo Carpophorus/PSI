db622cdad16bbc0da7e18631129699a60f141716
Marko Mrkonjić <markomrkonjic9@gmail.com>
2017-06-12 23:25:33 +0200

    index done

--- IMPL/src/Psi/AppBundle/Resources/views/App/index.html.php
+++ IMPL/src/Psi/AppBundle/Resources/views/App/index.html.php
@@ -2,4 +2,21 @@
 
 <?php $view['UI']->start('_content') ?>
 
+<div class="index-left">
+  <a href="search">
+    <span></span>
+    <div class="index-text">
+      SEARCH
+    </div>
+  </a>
+</div>
+<div class="index-right">
+  <a href="login">
+    <span></span>
+    <div class="index-text">
+      LOGIN
+    </div>
+  </a>
+</div>
+
 <?php $view['UI']->stop() ?>
